0fc94510d44b2994d2cb73948d061be75afa4093
itchyknee <pat.turner@itchyknee.co.uk>
2016-01-25 12:10:00 +0000

    fixing sig of PaymentClient

--- src/main/java/uk/nhsbsa/gds/hack/govpay/PaymentClient.java
+++ src/main/java/uk/nhsbsa/gds/hack/govpay/PaymentClient.java
@@ -1,16 +1,32 @@
 package uk.nhsbsa.gds.hack.govpay;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import uk.nhsbsa.gds.hack.model.Order;
+import uk.nhsbsa.gds.hack.model.Payment;
 
 public class PaymentClient {
 
+	private static String URL = "https://publicapi-integration-1.pymnt.uk/v1/";
+	
     @Autowired
     HttpComponentsClientHttpRequestFactory factory;
 
-    public Order getById(String paymentId) {
+    public Payment getById(String paymentId) {
+    	
+    	RestTemplate rest = new RestTemplate(factory);
+    	rest.getForObject("https://", Map.class);
+    	return null;
+    }
+    
+    public Payment create(Payment payment) {
+    	
+    	RestTemplate rest = new RestTemplate(factory);
+    	Payment result = rest.postForObject(URL + "payments", payment, Payment.class);
     	return null;
     }
     
