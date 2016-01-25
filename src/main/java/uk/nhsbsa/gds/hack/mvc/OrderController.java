760dc502ec921b7a5d4ec49ac27abe31dd0b6524
itchyknee <pat.turner@itchyknee.co.uk>
2016-01-25 13:48:05 +0000

    removed payment from order

--- src/main/java/uk/nhsbsa/gds/hack/mvc/OrderController.java
+++ src/main/java/uk/nhsbsa/gds/hack/mvc/OrderController.java
@@ -3,6 +3,7 @@ package uk.nhsbsa.gds.hack.mvc;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import uk.nhsbsa.gds.hack.data.IRepository;
 import uk.nhsbsa.gds.hack.model.Order;
-import uk.nhsbsa.gds.hack.model.Payment;
+import uk.nhsbsa.gds.hack.service.IOrderService;
 
 @Controller
 public class OrderController {
@@ -35,9 +36,11 @@ public class OrderController {
 
     @RequestMapping(value="/orders", method=POST)
     public String create(Model model, Order order) {
+    	
     	LOGGER.info("Creating new Order: {}", order);
-    	order.setPayment(new Payment());
+    	order.setId(UUID.randomUUID().toString());
     	orderRepo.add(order);
+    	LOGGER.info("Created new Order: {}", order);
     	return "redirect:/orders";
     }
 
